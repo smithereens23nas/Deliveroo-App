5b40d1904cd61a8f1d86bfc894cda6e0c9e41d10
smiethereens23nas <nicholas23smith@gmail.com>
2023-01-05 17:39:10 -0500

    first initial commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -9,3 +9,12 @@ export default function App() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}); 
